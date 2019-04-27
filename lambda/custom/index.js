89973afa73b379bd0d477641ed881361ee89ed67
SwapnilShahGit <swapnil@okera.com>
2019-04-27 13:10:48 -0700

    first working copy

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,333 +1,234 @@
-'use strict';
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
 
-const Alexa = require('alexa-sdk');
-const story = 'Escape the Office.html';
-const TableName = null // story.replace('.html','').replace(/\s/g, "-");
-var $twine = null;
-const linksRegex = /\[\[([^\|\]]*)\|?([^\]]*)\]\]/g;
+const Alexa = require('ask-sdk-core');
 
-module.exports.handler = (event, context, callback) => {
-  console.log(`handler: ${JSON.stringify(event.request)}`);
-
-  // read the Twine 2 (Harlowe) story into JSON
-  var fs = require('fs');
-  var contents = fs.readFileSync(story, 'utf8');
-  var m = contents.match(/<tw-storydata [\s\S]*<\/tw-storydata>/g);
-  var xml = m[0];
-  // because Twine xml has an attribute with no value
-  xml = xml.replace('hidden>', 'hidden="true">');
-  var parseString = require('xml2js').parseString;
-  parseString(xml, function(err, result) {
-    $twine = result['tw-storydata']['tw-passagedata'];
-  });
-
-  // prepare alexa-sdk
-  const alexa = Alexa.handler(event, context);
-  // APP_ID is your skill id which can be found in the Amazon developer console
-  // where you create the skill. Optionally set as a Lamba environment variable.
-  alexa.appId = process.env.APP_ID;
-  alexa.dynamoDBTableName = TableName;
-  alexa.registerHandlers(handlers);
-  alexa.execute();
-};
-
-const handlers = {
-  'LaunchRequest': function() {
-    console.log(`LaunchRequest`);
-    if (this.event.session.attributes['room'] !== undefined) {
-      var room = currentRoom(this.event);
-      var speechOutput = `Hello, you were playing before and got to the room called ${room['$']['name']}. Would you like to resume? `;
-      var reprompt = `Say, resume game, or, new game.`;
-      speechOutput = speechOutput + reprompt;
-      var cardTitle = `Restart`;
-      var cardContent = speechOutput;
-      var imageObj = undefined;
-      console.log(`LaunchRequest: ${JSON.stringify({
-        "speak": speechOutput,
-        "listen": reprompt,
-        "card" : {
-          "title": cardTitle,
-          "content": cardContent,
-          "imageObj": imageObj
-        }
-      })}`);
-      this.response.speak(speechOutput)
-        .listen(reprompt)
-        .cardRenderer(cardTitle, cardContent, imageObj);
-      this.emit(':responseReady');
-    } else {
-      this.emit('WhereAmI');
-    }
+const NameHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'NameIntent';
   },
-  'ResumeGame': function() {
-    console.log(`ResumeGame:`);
-    this.emit('WhereAmI');
-  },
-  'RestartGame': function() {
-    console.log(`RestartGame:`);
-    // clear session attributes
-    this.event.session.attributes['room'] = undefined;
-    this.event.session.attributes['visited'] = [];
-    this.emit('WhereAmI');
-  },
-  'WhereAmI': function() {
-    var speechOutput = "";
-    if (this.event.session.attributes['room'] === undefined) {
-      // you just started so you are in the first room
-      this.event.session.attributes['room'] = $twine[0]['$']['pid'];
-      speechOutput = `Welcome to ${story.replace('.html','')}. Lets start your game. `;
-    }
-
-    var room = currentRoom(this.event);
-    console.log(`WhereAmI: in ${JSON.stringify(room)}`);
-
-    // get displayable text
-    // e.g "You are here. [[Go South|The Hall]]" -> "You are here. Go South"
-    var displayableText = room['_'];
-    linksRegex.lastIndex = 0;
-    let m;
-    while ((m = linksRegex.exec(displayableText)) !== null) {
-      displayableText = displayableText.replace(m[0], m[1]);
-      linksRegex.lastIndex = 0;
-    }
-    // strip html
-    displayableText = displayableText.replace(/<\/?[^>]+(>|$)/g, "");
-    displayableText = displayableText.replace("&amp;", "and");
-    speechOutput = speechOutput + displayableText;
-
-    // create reprompt from links: "You can go north or go south"
-    var reprompt = "";
-    linksRegex.lastIndex = 0;
-    while ((m = linksRegex.exec(room['_'])) !== null) {
-      if (m.index === linksRegex.lastIndex) {
-        linksRegex.lastIndex++;
-      }
-      if (reprompt === "") {
-        if (!m[1].toLowerCase().startsWith('if you')) {
-          reprompt = "You can";
-        }
-      } else {
-        reprompt = `${reprompt} or`;
-      }
-      reprompt = `${reprompt} ${m[1]}`;
-    }
-
-    var firstSentence = displayableText.split('.')[0];
-    var lastSentence = displayableText.replace('\n',' ').split('. ').pop();
-    var reducedContent = `${firstSentence}. ${reprompt}.`;
-
-    // say less if you've been here before
-    if (this.event.session.attributes['visited'] === undefined) {
-      this.event.session.attributes['visited'] = [];
-    }
-    if (this.event.session.attributes['visited'].includes(room['$']['pid'])) {
-      console.log(`WhereAmI: player is revisiting`);
-      speechOutput = reducedContent;
-    } else {
-      this.event.session.attributes['visited'].push(room['$']['pid']);
-    }
-
-    var cardTitle = firstSentence;
-    var cardContent = (reprompt > '') ? reprompt : lastSentence;
-    var imageObj = undefined;
-
-    console.log(`WhereAmI: ${JSON.stringify({
-      "speak": speechOutput,
-      "listen": reprompt,
-      "card" : {
-        "title": cardTitle,
-        "content": cardContent,
-        "imageObj": imageObj
-      }
-    })}`);
-    linksRegex.lastIndex = 0;
-    if (linksRegex.exec(room['_'])) {
-      // room has links leading out, so listen for further user input
-      this.response.speak(speechOutput)
-        .listen(reprompt)
-        .cardRenderer(cardTitle, cardContent, imageObj);
-    } else {
-      console.log(`WhereAmI: at the end of a branch. Game over.`);
-      // clear session attributes
-      this.event.session.attributes['room'] = undefined;
-      this.event.session.attributes['visited'] = [];
-      this.response.speak(speechOutput)
-        .cardRenderer(cardTitle, cardContent, imageObj);
-    }
-    this.emit(':responseReady');
-  },
-  'Go': function() {
-    console.log(`Go`);
-    var slotValues = getSlotValues(this.event.request.intent.slots);
-    followLink(this.event, [slotValues['direction']['resolved'], slotValues['direction']['synonym']]);
-    this.emit('WhereAmI');
-  },
-  'Page': function() {
-    // old-school cyoa: "to go south turn to page 20"..you say, "page 20"
-    console.log(`Page`);
-    followLink(this.event, this.event.request.intent.slots.number.value);
-    this.emit('WhereAmI');
-  },
-  'Fight': function() {
-    console.log(`Fight`);
-    followLink(this.event, [this.event.request.intent.slots.npc.value, 'fight']);
-    this.emit('WhereAmI');
-  },
-  'AMAZON.HelpIntent': function() {
-    var speechOutput = 'This is the Sample Gamebook Skill. ';
-    var reprompt = 'Say where am I, to hear me speak.';
-    speechOutput = speechOutput + reprompt;
-    var cardTitle = 'Help.';
-    var cardContent = speechOutput;
-    var imageObj = undefined;
-    console.log(`HelpIntent: ${JSON.stringify({
-      "speak": speechOutput,
-      "listen": reprompt,
-      "card" : {
-        "title": cardTitle,
-        "content": cardContent,
-        "imageObj": imageObj
-      }
-    })}`);
-    this.response.speak(speechOutput)
-      .listen(reprompt)
-      .cardRenderer(cardTitle, cardContent, imageObj);
-    this.emit(':responseReady');
-  },
-  'AMAZON.CancelIntent': function() {
-    this.emit('CompletelyExit');
-  },
-  'AMAZON.StopIntent': function() {
-    this.emit('CompletelyExit');
-  },
-  'CompletelyExit': function() {
-    var speechOutput = 'Goodbye.';
-    if (TableName) {
-      speechOutput = `Your progress has been saved. ${speechOutput}`;
-    }
-    var cardTitle = 'Exit.';
-    var cardContent = speechOutput;
-    var imageObj = undefined;
-    console.log(`CompletelyExit: ${JSON.stringify({
-      "speak": speechOutput,
-      "listen": null,
-      "card" : {
-        "title": cardTitle,
-        "content": cardContent,
-        "imageObj": imageObj
-      }
-    })}`);
-    this.response.speak(speechOutput)
-      .cardRenderer(cardTitle, cardContent, imageObj);
-    this.emit(':responseReady');
-  },
-  'AMAZON.RepeatIntent': function() {
-    console.log(`RepeatIntent`);
-    this.emit('WhereAmI');
-  },
-  'Unhandled': function() {
-    // handle any intent in interaction model with no handler code
-    console.log(`Unhandled`);
-    followLink(this.event, this.event.request.intent.name);
-    this.emit('WhereAmI');
-  },
-  'SessionEndedRequest': function() {
-    // "exit", timeout or error. Cannot send back a response
-    console.log(`Session ended: ${this.event.request.reason}`);
+  handle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    var fname = request.intent.slots.fname.value;
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    attributes.fname = fname;
+    handlerInput.attributesManager.setSessionAttributes(attributes);
+    return handlerInput.responseBuilder
+      .speak(GREETING + " " + fname + ". " + DIFFICULTY_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
   },
 };
 
-function currentRoom(event) {
-  var currentRoomData = undefined;
-  for (var i = 0; i < $twine.length; i++) {
-    if ($twine[i]['$']['pid'] === event.session.attributes['room']) {
-      currentRoomData = $twine[i];
-      break;
-    }
-  }
-  return currentRoomData;
-}
+const DifficultyHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'DifficultyIntent';
+  },
+  handle(handlerInput) {
+    console.log("In difficulty handler");
+    const request = handlerInput.requestEnvelope.request;
+    var difficulty = request.intent.slots.difficulty.value;
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    attributes.difficulty = difficulty;
+    handlerInput.attributesManager.setSessionAttributes(attributes);
+    return handlerInput.responseBuilder
+      .speak("You have chosen difficulty " + difficulty + ". Let me choose a random category for you. I have chosen math. The question is What is five minus three?")
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
 
-function followLink(event, direction_or_array) {
-  var directions = [];
-  if (direction_or_array instanceof Array) {
-    directions = direction_or_array;
-  } else {
-    directions = [direction_or_array];
-  }
-  var room = currentRoom(event);
-  var result = undefined;
-  directions.every(function(direction, index, _arr) {
-    console.log(`followLink: try '${direction}' from ${room['$']['name']}`);
-    var directionRegex = new RegExp(`.*${direction}.*`, 'i');
-    let links;
-    linksRegex.lastIndex = 0;
-    while ((links = linksRegex.exec(room['_'])) !== null) {
-      if (links.index === linksRegex.lastIndex) {
-        linksRegex.lastIndex++;
-      }
-      result = links[1].match(directionRegex);
-      var target = links[2] || links[1];
-      console.log(`followLink: check ${links[1]} (${target}) for ${direction} => ${result} `);
-      if (result) {
-        console.log(`followLink: That would be ${target}`);
-        for (var i = 0; i < $twine.length; i++) {
-          if ($twine[i]['$']['name'].toLowerCase() === target.toLowerCase()) {
-            event.session.attributes['room'] = $twine[i]['$']['pid'];
-            break;
+const AnswerHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AnswerIntent';
+  },
+  handle(handlerInput) {
+    console.log("In answer handler");
+    const request = handlerInput.requestEnvelope.request;
+    var answer = request.intent.slots.answer.value;
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    var correct = false;
+    console.log(answer);
+    if (answer == 2 || answer == "two") {
+      console.log("answer is correct");
+      correct = true;
+    }
+    var reply = "";
+    console.log("created reply");
+    if (correct) {
+      reply = "You answered correctly!";
+    } else {
+      reply = "Incorrect answer!";
+    }
+    console.log(reply);
+    return handlerInput.responseBuilder
+      .speak(reply)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest'
+  },
+  handle(handlerInput) {
+    
+    return handlerInput.responseBuilder
+      .speak(GAME_SOUND1 + WELCOME_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
+
+const DailyGameHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest' && 
+      request.intent.name === 'DailyGame'&&
+        request.dialogState !== 'COMPLETED';
+  },
+  handle(handlerInput) {
+    
+    const request = handlerInput.requestEnvelope.request;
+    const filledSlots = request.intent.slots;
+    const answer = filledSlots.answerA.value;
+    
+    let currentQuestionCounter = 0;
+    let speechOutput = questionArray[currentQuestionCounter].question;
+    let correctAnswer = questionArray[currentQuestionCounter].answer;
+    
+    // if exists in session, we get currentQuestionCounter and previousAnswer
+    let currentIntent = handlerInput.requestEnvelope.request.intent;
+    const {attributesManager,responseBuilder} = handlerInput;
+    const sessionAttributes = attributesManager.getSessionAttributes();
+    if (sessionAttributes[currentIntent.name]) {
+      currentQuestionCounter = sessionAttributes[currentIntent.name].currentQuestionCounter;
+      correctAnswer =sessionAttributes[currentIntent.name].previousAnswer;
+      speechOutput = questionArray[currentQuestionCounter].question;
+      console.log("currentQuestionCounter" + currentQuestionCounter);
+    }
+    if (answer){
+      if (answer.toLowerCase() === correctAnswer) {
+        speechOutput = CORRECT_ANSWER;
+        if (currentQuestionCounter < questionArray.length-1){
+          speechOutput = speechOutput + NEXT_QUESTION_MESSAGE + questionArray[currentQuestionCounter+1].question;
+          correctAnswer = questionArray[currentQuestionCounter+1].answer;
+        }else{
+            speechOutput = speechOutput + CONGRATS_MESSAGE;
           }
+        currentQuestionCounter++;
         }
-        break;
-      }
+        else {
+          speechOutput = INCORRECT_ANSWER +" "+ speechOutput;
+        }
     }
-    return !result;
-  });
-}
+    // Saving correctAnswer and counter into session
+    let previousAnswer = correctAnswer;
+    sessionAttributes[currentIntent.name] = {currentQuestionCounter,previousAnswer};
+    attributesManager.setSessionAttributes(sessionAttributes);
+    
+    var response =  handlerInput.responseBuilder
+      .speak(speechOutput)
+      .reprompt(speechOutput)
+      .addElicitSlotDirective("answerA")
+      .getResponse();
+    return response;
+  },
+};
 
-//COOKBOOK HELPER FUNCTIONS
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
 
-function getSlotValues(filledSlots) {
-  //given event.request.intent.slots, a slots values object so you have
-  //what synonym the person said - .synonym
-  //what that resolved to - .resolved
-  //and if it's a word that is in your slot values - .isValidated
-  let slotValues = {};
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(STOP_MESSAGE)
+      .getResponse();
+  },
+};
 
-  console.log('The filled slots: ' + JSON.stringify(filledSlots));
-  Object.keys(filledSlots).forEach(function(item) {
-    //console.log("item in filledSlots: "+JSON.stringify(filledSlots[item]));
-    var name = filledSlots[item].name;
-    //console.log("name: "+name);
-    if (filledSlots[item] &&
-      filledSlots[item].resolutions &&
-      filledSlots[item].resolutions.resolutionsPerAuthority[0] &&
-      filledSlots[item].resolutions.resolutionsPerAuthority[0].status &&
-      filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`The session ended: ${handlerInput.requestEnvelope.request.reason}`);
 
-      switch (filledSlots[item].resolutions.resolutionsPerAuthority[0].status.code) {
-        case "ER_SUCCESS_MATCH":
-          slotValues[name] = {
-            "synonym": filledSlots[item].value,
-            "resolved": filledSlots[item].resolutions.resolutionsPerAuthority[0].values[0].value.name,
-            "isValidated": true
-          };
-          break;
-        case "ER_SUCCESS_NO_MATCH":
-          slotValues[name] = {
-            "synonym": filledSlots[item].value,
-            "resolved": filledSlots[item].value,
-            "isValidated": false
-          };
-          break;
-      }
-    } else {
-      slotValues[name] = {
-        "synonym": filledSlots[item].value,
-        "resolved": filledSlots[item].value,
-        "isValidated": false
-      };
-    }
-  }, this);
-  //console.log("slot values: " + JSON.stringify(slotValues));
-  return slotValues;
-}
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('<say-as interpret-as="interjection">ouch</say-as> there was a problem with your request')
+      .reprompt('there was a problem with your request')
+      .getResponse();
+  },
+};
+
+const DIFFICULTY_MESSAGE = "What level of difficulty would you like? Please say easy medium or hard";
+const GREETING = "Hello"
+const SKILL_NAME = "STEM party";
+const WELCOME_MESSAGE = " Welcome to STEM party!. What is your name?";
+const GAME_SOUND1 = "<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_player1_01'/>";
+const HELP_MESSAGE = "You can start your daily game, or check how you compare with your friends... ¿How can I help?";
+const HELP_REPROMPT = "How can I help?";
+const CORRECT_ANSWER = "Correct!";
+const INCORRECT_ANSWER = "Incorrect!";
+const NEXT_QUESTION_MESSAGE = "Next!";
+const CONGRATS_MESSAGE = "Congratulations, the trivia has ended!";
+const STOP_MESSAGE = "<say-as interpret-as='interjection'>okey dokey</say-as><s> see you later </s>";
+
+const skillBuilder = Alexa.SkillBuilders.custom();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    AnswerHandler,
+    NameHandler,
+    DifficultyHandler,
+    DailyGameHandler,
+    HelpHandler,
+    ExitHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
+
+const questionArray = [
+ {"question":"What city is re:invent 2018 in?", "answer":"las vegas" },
+ {"question":"How many attendees are expected in re:invent 2018?", "answer":"43000" },
+ {"question":"What is the name of this session in Vegas?", "answer":"cool session" }
+];
